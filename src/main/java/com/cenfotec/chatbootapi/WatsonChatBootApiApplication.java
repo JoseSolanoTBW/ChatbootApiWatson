91b88ab65dc04ab3c56554fb1c4e630e4c17f7d0
Jose Solano <dsolano@builderhomesite.com>
2018-12-04 21:37:47 -0600

    Converting to a War instead a JAR

--- src/main/java/com/cenfotec/chatbootapi/WatsonChatBootApiApplication.java
+++ src/main/java/com/cenfotec/chatbootapi/WatsonChatBootApiApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = RepositoryRestMvcAutoConfiguration.class)
-public class WatsonChatBootApiApplication {
+public class WatsonChatBootApiApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WatsonChatBootApiApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WatsonChatBootApiApplication.class, args);
 	}
